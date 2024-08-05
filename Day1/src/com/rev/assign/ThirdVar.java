4f824b061dbdf29044d77cc126b8c29e1ccac157
AashleshaKondgire <99065261+AashleshaKondgire@users.noreply.github.com>
2024-08-05 14:08:12 +0530

    Add files via upload

---
+++ Day1/src/com/rev/assign/ThirdVar.java
@@ -0,0 +1,14 @@
+package com.rev.assign;
+
+public class ThirdVar {
+	public static void main(String[] args) {
+		int a=3;
+		int b=4;
+		int temp = a;
+		a = b;
+		b = temp ;
+		System.out.println("Value of a : " + a + " value of b: "+ b);
+	}
+
+
+}
